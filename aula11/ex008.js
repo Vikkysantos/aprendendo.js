48f4dd98af93d2f9aa604b0857623bc40ea2087c
Vitoria Santoss <vicksantos348@gmail.com>
2025-06-17 00:05:15 +0000

    adicionar novos excercicios

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 12;
+console.log(`a velocidade do carro é ${vel} km por hora`);
+if (vel > 60) {
+  /* condição simples */
+  console.log(`voce ultrapassou o limite`);
+}
+
+console.log(`dirija com cuidado`);
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var País = EUA;
+console.log(`vivendo em ${País}`);
+if (País == "Brasil") {
+  console.log(` brasileiro`);
+} else {
+  console.log(`estrangeiro`);
+}
